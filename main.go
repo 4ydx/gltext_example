28ff0c2a6acf03f36360f69275f27e832437ec46
nate <nathan@4ydx.com>
2015-12-12 15:14:18 +0900

    Example using Japanese text that creates an image using ranges: http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 	fmt.Println("Opengl version", version)
 
 	// code from here
-	gltext.IsDebug = true
+	gltext.IsDebug = false
 
 	fd, err := os.Open("font/font_1_honokamin.ttf")
 	if err != nil {
@@ -51,15 +51,31 @@ func main() {
 	}
 	defer fd.Close()
 
-	// ascii chars from 32 to 127
+	// Japanese character ranges
+	// http://www.rikai.com/library/kanjitables/kanji_codes.unicode.shtml
+	// http://www.binaryhexconverter.com/hex-to-decimal-converter
+	// 3000 - 3030 -> 12288 - 12336
+	// 3040 - 309f -> 12352 - 12447
+	// 30a0 - 30ff -> 12448 - 12543
+	// 4e00 - 9faf -> 19968 - 40879
+	// ff00 - ffef -> 65280 - 65519
 	scale := fixed.Int26_6(32)
 	runesPerRow := fixed.Int26_6(128)
+	runeRanges := make(gltext.RuneRanges, 0)
+	runeRange := gltext.RuneRange{Low: 12288, High: 12336}
+	runeRanges = append(runeRanges, runeRange)
+	runeRange = gltext.RuneRange{Low: 12352, High: 12447}
+	runeRanges = append(runeRanges, runeRange)
+	runeRange = gltext.RuneRange{Low: 12448, High: 12543}
+	runeRanges = append(runeRanges, runeRange)
+	runeRange = gltext.RuneRange{Low: 19968, High: 40879}
+	runeRanges = append(runeRanges, runeRange)
 
 	font, err := gltext.LoadTruetype("fontconfigs")
 	if err == nil {
 		fmt.Println("Font loaded from disk...")
 	} else {
-		font, err = gltext.NewTruetype(fd, scale, 12000, 30000, runesPerRow)
+		font, err = gltext.NewTruetype(fd, scale, runeRanges, runesPerRow)
 		if err != nil {
 			panic(err)
 		}
@@ -71,9 +87,9 @@ func main() {
 
 	scaleMin, scaleMax := float32(1.0), float32(1.1)
 	text := gltext.NewText(font, scaleMin, scaleMax)
-	str := "梅干しが大好き"
+	str := "梅干しが大好き。ウメボシガダイスキ。"
 	for _, s := range str {
-		fmt.Println("%c: %d\n", s, rune(s))
+		fmt.Printf("%c: %d\n", s, rune(s))
 	}
 	text.SetString(str)
 
